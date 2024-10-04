e0ebb8efa05e90c604e76e3359cb3a467488180a
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-04 17:24:12 -0400

    [add] disfluency predictor

---
+++ src/fluencyfeatureannotator/modules/models/disfluency_predictor/disfluency_predictor.py
@@ -0,0 +1,25 @@
+from modules.common import RESOURCES_PATH
+from modules.models.utils.base import DistilBertTokenClassifierBase, RobertaTokenClassifierBase
+
+FINETUED_DISFLUENCY_PREDICTOR_BERT = RESOURCES_PATH / "pretrained_model/finetuned_disfluency_predictor_v1"
+FINETUED_DISFLUENCY_PREDICTOR_ROBERTA = RESOURCES_PATH / "pretrained_model/finetuned_disfluency_predictor_v3"
+FINETUED_DISFLUENCY_PREDICTOR_ROBERTA_L1 = RESOURCES_PATH / "pretrained_model/finetuned_disfluency_predictor_v4"
+
+class DisfluencyPredictorBert(DistilBertTokenClassifierBase):
+    def __init__(self, n_class: int =8, device: str ="cpu") -> 'DisfluencyPredictorBert':
+        super().__init__(n_class=n_class, device=device)
+
+    @classmethod
+    def from_finetuned(cls, device: str ="cpu") -> 'DisfluencyPredictorBert':
+        return super().from_finetuned(FINETUED_DISFLUENCY_PREDICTOR_BERT, device=device)
+
+class DisfluencyPredictorRoberta(RobertaTokenClassifierBase):
+    def __init__(self, n_class: int =2, ignore_tag: int =-100, device: str ="cpu") -> 'DisfluencyPredictorRoberta':
+        super().__init__(n_class=n_class, ignore_tag=ignore_tag, device=device)
+
+    @classmethod
+    def from_finetuned(cls, device: str ="cpu", L1: bool =False) -> 'DisfluencyPredictorRoberta':
+        if L1:
+            return super().from_finetuned(FINETUED_DISFLUENCY_PREDICTOR_ROBERTA_L1, device=device)
+
+        return super().from_finetuned(FINETUED_DISFLUENCY_PREDICTOR_ROBERTA, device=device)
