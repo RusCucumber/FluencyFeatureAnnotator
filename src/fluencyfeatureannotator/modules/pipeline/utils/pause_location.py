9f4a3621054c7627f477223cb74d1d07ca512e53
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-04 18:06:56 -0400

    [add] pause locations

---
+++ src/fluencyfeatureannotator/modules/pipeline/utils/pause_location.py
@@ -0,0 +1,9 @@
+from enum import Enum, auto
+
+
+class PauseLocation(Enum):
+    CLAUSE_INTERNAL = auto()
+    CLAUSE_EXTERNAL = auto()
+    BEFORE_TURN = auto()
+    AFTER_TURN = auto()
+    CLAUSE_COVERED = auto()
