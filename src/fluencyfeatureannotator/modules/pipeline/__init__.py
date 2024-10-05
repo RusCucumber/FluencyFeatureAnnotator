37757c0bcb769a166c3a374ad65f496b30838122
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-04 20:43:43 -0400

    [add] modules

---
+++ src/fluencyfeatureannotator/modules/pipeline/__init__.py
@@ -0,0 +1,6 @@
+from modules.pipeline.asr import ASR  # noqa: F401
+from modules.pipeline.dependency_parser import SpacyClauseDetector, StanzaClauseDetector  # noqa: F401
+from modules.pipeline.pause_location_detector import PauseLocationDetector  # noqa: F401
+from modules.pipeline.pipeline import Annotator  # noqa: F401
+from modules.pipeline.pruning import Pruning  # noqa: F401
+from modules.pipeline.sentence_end_detector import SentenceEndDetector  # noqa: F401
