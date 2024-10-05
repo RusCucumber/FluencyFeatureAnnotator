484d834799a3331636e51fcbffd1545f8330fb45
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-05 01:01:05 -0400

    [fix] RESOURCES_PATH

--- src/fluencyfeatureannotator/modules/common/__init__.py
+++ src/fluencyfeatureannotator/modules/common/__init__.py
@@ -5,4 +5,6 @@ from modules.common.gcp import GCSBucket  # noqa: F401
 from modules.common.turn import Turn, shorten_turn  # noqa: F401
 from modules.common.word import DisfluencyEnum, DisfluencyWord, Word  # noqa: F401
 
-RESOURCES_PATH = Path("modules/resources").resolve()
+RESOURCES_PATH = Path.cwd() / "src/fluencyfeatureannotator/modules/resources"
+if not RESOURCES_PATH.exists():
+    RESOURCES_PATH = Path(__file__).parents[2] / "modules/resources"
