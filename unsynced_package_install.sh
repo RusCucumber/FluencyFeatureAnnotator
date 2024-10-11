59893ea651e5e0e44abe370c84619b7a76e83d22
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-11 18:17:07 -0400

    [add] package installer

---
+++ unsynced_package_install.sh
@@ -0,0 +1,3 @@
+rye run pip install k2==1.24.4.dev20240905+cpu.torch2.4.1 -f https://k2-fsa.github.io/k2/cpu.html
+cd src/fluencyfeatureannotator/modules/resources
+pip install ./en_ud_L1L2e_combined_trf-0.0.1/
