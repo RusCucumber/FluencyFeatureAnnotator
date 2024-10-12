60c60930620e8541570af3df6b03d09fbcc2489e
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-12 02:32:36 -0400

    [upd] add file selection warning banner

--- src/fluencyfeatureannotator/main.py
+++ src/fluencyfeatureannotator/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import Callable, List
 
 import flet as ft
 import pandas as pd
@@ -29,6 +29,21 @@ class SelectedFileContainer(ft.Container):
 
         self.content = self.selected_file_list
 
+class FileSelectionWarningBanner(ft.Banner):
+    def __init__(self, on_click: Callable):
+        content = ft.Text(
+            value="Select wav and txt file pairs!",
+            color=ft.colors.BLACK,
+        )
+        actions = [
+            ft.TextButton(text="Close", style=ft.ButtonStyle(color=ft.colors.PRIMARY), on_click=on_click)
+        ]
+
+        super().__init__(content=content, actions=actions)
+
+        self.bgcolor = ft.colors.AMBER_100
+        self.leading = ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.AMBER, size=40)
+
 class WavTxtFilePicker(ft.FilePicker):
     def __init__(
         self,
@@ -41,6 +56,10 @@ class WavTxtFilePicker(ft.FilePicker):
         self.picked_txt_path_list = []
 
         self.selected_file_container = selected_file_container
+        self.file_selection_warning_banner = FileSelectionWarningBanner(on_click=self.close_banner)
+
+    def close_banner(self, e):
+        self.page.close(self.file_selection_warning_banner)
 
     def is_wav_txt_path_pair_picked(
         self,
@@ -81,6 +100,7 @@ class WavTxtFilePicker(ft.FilePicker):
                 picked_txt_path_list.append(file_path)
 
         if not self.is_wav_txt_path_pair_picked(picked_wav_path_list, picked_txt_path_list):
+            self.page.open(self.file_selection_warning_banner)
             self.selected_file_container.selected_file_list.controls = [
                 NO_FILE_SELECTED_TEXT
             ]
