fd5b2291f5f60bd64529feb92167c2bba095cda1
RusCucumber <w.d.ryu.m@gmail.com>
2024-10-11 21:06:43 -0400

    [add] fluecy feature annotator

---
+++ src/fluencyfeatureannotator/fluency_feature_annotator.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+from traceback import format_exc
+from typing import Generator, List, Tuple
+
+import pandas as pd
+from modules import Annotator, DisfluencyEnum, Turn, UtteranceFluencyMeasureExtractor
+from modules.forced_alignment.fa import LessPeakyCTCFA
+from modules.forced_alignment.utils.rev.df_2_rev import df_2_rev
+from modules.pipeline.utils.pause_location import PauseLocation
+from modules.utils.rev_utils import FILLER, transcript_2_df
+from rev_ai import Transcript
+from textgrids import TextGrid
+
+
+class FluencyFeatureAnnotator:
+    def __init__(self) -> None:
+        self.fa = LessPeakyCTCFA()
+        self.annotator = Annotator(process=["eos_detect", "pruning", "clause_detect"])
+
+    def preprocess_for_turn(self, rev_transcript: Transcript) -> Tuple[pd.DataFrame, List[int]]:
+        df_rev = transcript_2_df(rev_transcript)
+
+        mask = [not(flag) for flag in df_rev["text"].str.contains("<.*?>", regex=True, na=True)]
+        df_rev = df_rev[mask] # inaudible, laughter 等を除去
+
+        filler_locations = [] # フィラーの word id のリスト
+        idx = -1
+        for i in df_rev.index:
+            w = df_rev.at[i, "text"]
+            t = df_rev.at[i, "type"]
+
+            if t == "text":
+                idx += 1
+                if w.lower() in FILLER:
+                    filler_locations.append(idx)
+                if " " in w:
+                    w = w.replace(" ", "_")
+                    df_rev.at[i, "text"] = w
+
+        df_text = df_rev[df_rev["type"] != "punct"].reset_index()
+        df_text["text"] = df_text["text"].str.lower()
+
+        return df_text, filler_locations
+
+    def df_2_turn(self, df_rev: pd.DataFrame, filler_locations: List[int]) -> Turn:
+        turn = Turn.from_DataFrame(df_rev, 0, word_col="text")
+
+        disfluency_list = [DisfluencyEnum.FILLER for _ in filler_locations]
+        turn.clauses[0].annotate_disfluency(filler_locations, disfluency_list)
+        turn.reset_words()
+
+        return turn
+
+    def find_pauses(seflf, turn: Turn) -> List[dict]:
+        pauses = []
+        prev_clause_end = turn.start_time
+        for clause in turn.clauses:
+            if clause.start_time - prev_clause_end >= 0.25:
+                p = {
+                    "location": PauseLocation.CLAUSE_EXTERNAL,
+                    "start_time": prev_clause_end,
+                    "end_time": clause.start_time
+                }
+                pauses.append(p)
+
+            prev_word_end = clause.start_time
+            for word in clause.words:
+                if word.start_time - prev_word_end >= 0.25:
+                    p = {
+                        "location": PauseLocation.CLAUSE_INTERNAL,
+                        "start_time": prev_word_end,
+                        "end_time": word.start_time
+                    }
+                    pauses.append(p)
+
+                prev_word_end = word.end_time
+
+            prev_clause_end = clause.end_time
+
+        return pauses
+
+    def wav_txt_file_path_generator(
+        self,
+        wav_file_path_list: List[Path],
+        txt_file_path_list: List[Path]
+    ) -> Generator[Tuple[Path, Path], None, None]:
+        for wav_file_path in wav_file_path_list:
+            for txt_file_path in txt_file_path_list:
+                if txt_file_path.stem != wav_file_path.stem:
+                    continue
+
+                yield wav_file_path, txt_file_path
+                break
+
+    def annotate(
+        self,
+        wav_file_path_list: List[Path],
+        txt_file_path_list: List[Path]
+    ) -> Tuple[Turn, TextGrid]:
+        for file_path in self.wav_txt_file_path_generator(wav_file_path_list, txt_file_path_list):
+            wav_file_path, txt_file_path = file_path
+
+            with open(txt_file_path, "r") as f:
+                transcript = f.readline()
+
+            try:
+                df_fa = self.fa.align(wav_file_path, transcript, visualize_result=False)
+                rev_transcript = df_2_rev(df_fa)
+                rev_transcript = Transcript.from_json(rev_transcript)
+            except Exception:
+                print(format_exc())
+                return
+
+            df_transcript, filler_location = self.preprocess_for_turn(rev_transcript)
+            turn = self.df_2_turn(df_transcript, filler_location)
+
+            turn.ignore_disfluency()
+
+            try:
+                turn = self.annotator(turn=turn)
+            except Exception:
+                print(format_exc())
+                return
+
+            turn.show_disfluency()
+
+            pauses = self.find_pauses(turn)
+            grid = self.annotator.to_textgrid(turn, pauses)
+
+            return turn, grid
+
+    def extract(self, turn: Turn, grid: TextGrid):
+        extractor = UtteranceFluencyMeasureExtractor()
+
+        features = extractor.extract_by_turn(turn, grid)
+        feature_names = extractor.check_feature_names()
+
+        return features, feature_names
