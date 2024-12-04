d18d55b587f733a5d4d9083cdcb7118991ef8ea6
RusCucumber <w.d.ryu.m@gmail.com>
2024-12-04 12:50:47 -0500

    [upd] delivery

--- src/fluencyfeatureannotator/modules/__init__.py
+++ src/fluencyfeatureannotator/modules/__init__.py
@@ -2,3 +2,4 @@ from modules.common import *  # noqa: F403
 from modules.feature_extractor import *  # noqa: F403
 from modules.models import *  # noqa: F403
 from modules.pipeline import *  # noqa: F403
+
